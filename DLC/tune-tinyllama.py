2dcfefc648bac73b9a17a3a5b3753af6a2e71c6a
chihuan66 <chihuan@chipltech.com>
2025-12-12 17:57:00 +0800

    fix: 1212

--- DLC/tune-tinyllama.py
+++ DLC/tune-tinyllama.py
@@ -149,7 +149,7 @@ class KernelFlagsTuner(MeasurementInterface):
     test_ready_count_lock.acquire()
     # only one thread is allowed to run the model
     if self.is_executor:
-      run_cmd = "DLC_VISIBLE_DEVICES=4 ACCELERATE_TORCH_DEVICE=dlc python3 sft_trainer.py --device=dlc \
+      run_cmd = "DLC_VISIBLE_DEVICES=0 ACCELERATE_TORCH_DEVICE=dlc python3 sft_trainer.py --device=dlc \
                 --model=./tinyllama-15M --max_seq_length=512 --dtype=bfloat16 --dropout=0.05"
       print("Executor starts to run the model")
       os.chdir(get_llama_path())
